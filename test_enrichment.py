f110bfc628e4ed2a96cc52cd485727bb280ac1bf
KIRTAN <kirtanpatel24399@gmail.com>
2020-06-01 12:26:41 +0530

    working_on_enrichment_3

--- test_enrichment.py
+++ test_enrichment.py
@@ -8,27 +8,12 @@ def ML(x_train,y_train,given_weights):
 
     image_size = 784 # 28*28
     num_classes = 10 # ten unique digits
-
     model = Sequential()
-
-    # The input layer requires the special input_shape parameter which should match
-    # the shape of our training data.
     model.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
     model.add(Dense(units=num_classes, activation='softmax'))
     model.set_weights(given_weights)
-    # model.summary()
 
     model.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
     history = model.fit(x_train, y_train, batch_size=128, epochs=5, verbose=False, validation_split=.1)
     weights = model.get_weights()
-    # plt.plot(history.history['accuracy'])
-    # plt.plot(history.history['val_accuracy'])
-    # plt.title('model accuracy')
-    # plt.ylabel('accuracy')
-    # plt.xlabel('epoch')
-    # plt.legend(['training', 'validation'], loc='best')
-    # plt.show()
-    # keras.backend.clear_session()
-    # print(f'Test loss: {loss:.3}')
-    # print(f'Test accuracy: {accuracy:.3}')
     return [weights,history]
--- test_enrichment2.py
+++ test_enrichment2.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from keras.datasets import mnist
 from keras.layers import Dense # Dense layers are "fully connected" layers
 from keras.models import Sequential # Documentation: https://keras.io/models/sequential/
-from neuralnetwork import ML
-
+from encryptedneuralnetwork import ML
+from phe import paillier
 #<------------------------------------Self Training-------------------------------->
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
+pub_key,priv_key = paillier.generate_paillier_keypair()
 
 image_vector_size = 28*28
 x_train = x_train.reshape(x_train.shape[0], image_vector_size)
@@ -25,68 +25,78 @@ model1 = Sequential()
 model1.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
 model1.add(Dense(units=num_classes, activation='softmax'))
 model1.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
-history1 = model1.fit(x_train[0:10000], y_train[0:10000], batch_size=128, epochs=5, verbose=False, validation_split=.1)
+model1.fit(x_train[0:100], y_train[0:100], batch_size=128, epochs=1, verbose=False, validation_split=.1)
 loss1, accuracy1  = model1.evaluate(x_test, y_test, verbose=False)
-weights1 = model1.get_weights()
+weights_old = model1.get_weights()
 print(f'Test loss: {loss1:.3}')
 print(f'Test accuracy: {accuracy1:.3}')
 
-#<------------------------------------Calling enhancer  1-------------------------------->
-model2 = Sequential()
-model2.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
-model2.add(Dense(units=num_classes, activation='softmax'))
-[weights2,history2] = ML(x_train[10000:20000,:],y_train[10000:20000,:],weights1) 
-model2.set_weights(weights2)
 
-model2.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
-loss2, accuracy2  = model2.evaluate(x_test, y_test, verbose=False)
-print(f'Test loss: {loss2:.3}')
-print(f'Test accuracy: {accuracy2:.3}')
+datalevels = [0,10000,20000,35000,50000,60000]
+listofweights = []
+for iterations in range(10):
+    listofweights.clear()
+    for i in range(5):
+        print('calling', i)
+        weights_new = ML(x_train[datalevels[i]:datalevels[i+1],:],y_train[datalevels[i]:datalevels[i+1],:],weights_old,pub_key,priv_key) 
+        print('encrypted weights received')
+        listofweights.append(weights_new)
+    count=0
+    j=k=0
+    print('averaging weights....')
+    for j in range(784):
+        for k in range(32):
+            temp0 = paillier.EncryptedNumber(pub_key,int(listofweights[0][count][0]),int(listofweights[0][count][1]))
+            temp1 = paillier.EncryptedNumber(pub_key,int(listofweights[1][count][0]),int(listofweights[1][count][1]))
+            temp2 = paillier.EncryptedNumber(pub_key,int(listofweights[2][count][0]),int(listofweights[2][count][1]))
+            temp3 = paillier.EncryptedNumber(pub_key,int(listofweights[3][count][0]),int(listofweights[3][count][1]))
+            temp4 = paillier.EncryptedNumber(pub_key,int(listofweights[4][count][0]),int(listofweights[4][count][1]))
+            temp5 = temp0.__add__(temp1).__add__(temp2).__add__(temp3).__add__(temp4)
 
-#<------------------------------------Calling enhancer  2-------------------------------->
-model3 = Sequential()
-model3.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
-model3.add(Dense(units=num_classes, activation='softmax'))
-[weights3,history3] = ML(x_train[20000:30000,:],y_train[20000:30000,:],weights2) 
-model3.set_weights(weights3)
+            count = count + 1
+            weights_old[0][j][k] = priv_key.decrypt(temp5)
+            weights_old[0][j][k] = weights_old[0][j][k]/5
+    j=0
+    for j in range(32):
+            temp0 = paillier.EncryptedNumber(pub_key,int(listofweights[0][count][0]),int(listofweights[0][count][1]))
+            temp1 = paillier.EncryptedNumber(pub_key,int(listofweights[1][count][0]),int(listofweights[1][count][1]))
+            temp2 = paillier.EncryptedNumber(pub_key,int(listofweights[2][count][0]),int(listofweights[2][count][1]))
+            temp3 = paillier.EncryptedNumber(pub_key,int(listofweights[3][count][0]),int(listofweights[3][count][1]))
+            temp4 = paillier.EncryptedNumber(pub_key,int(listofweights[4][count][0]),int(listofweights[4][count][1]))
+            temp5 = temp0.__add__(temp1).__add__(temp2).__add__(temp3).__add__(temp4)
+            count = count + 1
+            weights_old[1][j] = priv_key.decrypt(temp5)
+            weights_old[1][j] = weights_old[1][j]/5
+    j=k=0
+    for j in range(32):
+        for k in range(10):
+            temp0 = paillier.EncryptedNumber(pub_key,int(listofweights[0][count][0]),int(listofweights[0][count][1]))
+            temp1 = paillier.EncryptedNumber(pub_key,int(listofweights[1][count][0]),int(listofweights[1][count][1]))
+            temp2 = paillier.EncryptedNumber(pub_key,int(listofweights[2][count][0]),int(listofweights[2][count][1]))
+            temp3 = paillier.EncryptedNumber(pub_key,int(listofweights[3][count][0]),int(listofweights[3][count][1]))
+            temp4 = paillier.EncryptedNumber(pub_key,int(listofweights[4][count][0]),int(listofweights[4][count][1]))
+            temp5 = temp0.__add__(temp1).__add__(temp2).__add__(temp3).__add__(temp4)
+            count = count + 1
+            weights_old[2][j][k] = priv_key.decrypt(temp5)
+            weights_old[2][j][k] = weights_old[2][j][k]/5
+    j=0
+    for j in range(10):
+            temp0 = paillier.EncryptedNumber(pub_key,int(listofweights[0][count][0]),int(listofweights[0][count][1]))
+            temp1 = paillier.EncryptedNumber(pub_key,int(listofweights[1][count][0]),int(listofweights[1][count][1]))
+            temp2 = paillier.EncryptedNumber(pub_key,int(listofweights[2][count][0]),int(listofweights[2][count][1]))
+            temp3 = paillier.EncryptedNumber(pub_key,int(listofweights[3][count][0]),int(listofweights[3][count][1]))
+            temp4 = paillier.EncryptedNumber(pub_key,int(listofweights[4][count][0]),int(listofweights[4][count][1]))
+            temp5 = temp0.__add__(temp1).__add__(temp2).__add__(temp3).__add__(temp4)
+            count = count + 1
+            weights_old[3][j] = priv_key.decrypt(temp5)
+            weights_old[3][j] = weights_old[3][j]/5
 
-model3.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
-loss3, accuracy3  = model3.evaluate(x_test, y_test, verbose=False)
-print(f'Test loss: {loss3:.3}')
-print(f'Test accuracy: {accuracy3:.3}')
-
-#<------------------------------------Calling enhancer  3-------------------------------->
-model4 = Sequential()
-model4.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
-model4.add(Dense(units=num_classes, activation='softmax'))
-[weights4,history4] = ML(x_train[30000:40000,:],y_train[30000:40000,:],weights3) 
-model4.set_weights(weights4)
-
-model4.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
-loss4, accuracy4  = model4.evaluate(x_test, y_test, verbose=False)
-print(f'Test loss: {loss4:.3}')
-print(f'Test accuracy: {accuracy4:.3}')
-
-#<------------------------------------Calling enhancer  4-------------------------------->
-model5 = Sequential()
-model5.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
-model5.add(Dense(units=num_classes, activation='softmax'))
-[weights5,history5] = ML(x_train[40000:45000,:],y_train[40000:45000,:],weights4) 
-model5.set_weights(weights5)
-
-model5.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
-loss5, accuracy5  = model5.evaluate(x_test, y_test, verbose=False)
-print(f'Test loss: {loss5:.3}')
-print(f'Test accuracy: {accuracy5:.3}')
-
-#<------------------------------------Calling enhancer  5-------------------------------->
-model6 = Sequential()
-model6.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
-model6.add(Dense(units=num_classes, activation='softmax'))
-[weights6,history6] = ML(x_train[45000:,:],y_train[45000:,:],weights5) 
-model6.set_weights(weights6)
-
-model6.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
-loss6, accuracy6  = model6.evaluate(x_test, y_test, verbose=False)
-print(f'Test loss: {loss6:.3}')
-print(f'Test accuracy: {accuracy6:.3}')
+    print('testing average model')
+    model2 = Sequential()
+    model2.add(Dense(units=32, activation='sigmoid', input_shape=(image_size,)))
+    model2.add(Dense(units=num_classes, activation='softmax'))
+    model2.set_weights(weights_old)
+    model2.compile(optimizer="sgd", loss='categorical_crossentropy', metrics=['accuracy'])
+    loss2, accuracy2  = model2.evaluate(x_test, y_test, verbose=False)
+    print(iterations)
+    print(f'Test accuracy: {accuracy2:.3}')
